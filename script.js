76c0e2dc482dc6f10545fc5fcf08cb1b132acb29
SamyakJain789 <108120140+SamyakJain789@users.noreply.github.com>
2023-03-30 18:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 function calculateMinCost() {
   //your code here
-   arr.sort((a, b) => a - b); // Sort the array in non-decreasing order
-  let closestSum = Infinity;
-  for (let i = 0; i < arr.length - 2; i++) {
-    let j = i + 1;
-    let k = arr.length - 1;
-    while (j < k) {
-      const sum = arr[i] + arr[j] + arr[k];
-      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-        closestSum = sum;
-      }
-      if (sum < target) {
-        j++;
-      } else {
-        k--;
-      }
-    }
-  }
-  return closestSum;
-}
+var res = 0;
+	var cost = 0;
+	var inputArray = document.getElementById("rope-lengths").value;
+
+	inputArray = inputArray.split(',').map(Number);
+	console.log(inputArray);
+	for(let i = 0;i< inputArray.length -1;i+=0) {
+		if (inputArray.length == 0) break;
+		inputArray = inputArray.sort((a,b) => b-a);
+		res = inputArray.pop()+ inputArray.pop();
+		cost += res;
+		inputArray[inputArray.length] = res; }
+
+	document.getElementById("result").innerHTML = cost;
+	}
   
-module.exports = threeSum;
