bd997b620115a3ce900549d48e1fbc5a7f01e570
SamyakJain789 <108120140+SamyakJain789@users.noreply.github.com>
2023-03-30 18:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function calculateMinCost() {
   //your code here
+   arr.sort((a, b) => a - b); // Sort the array in non-decreasing order
+  let closestSum = Infinity;
+  for (let i = 0; i < arr.length - 2; i++) {
+    let j = i + 1;
+    let k = arr.length - 1;
+    while (j < k) {
+      const sum = arr[i] + arr[j] + arr[k];
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      if (sum < target) {
+        j++;
+      } else {
+        k--;
+      }
+    }
+  }
+  return closestSum;
+}
   
-  
-  
-}  
+module.exports = threeSum;
